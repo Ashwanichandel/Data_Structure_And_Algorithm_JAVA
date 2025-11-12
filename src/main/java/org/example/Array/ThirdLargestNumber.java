faff581af52abd0ea2e4371ff5e5e39b28e68efd
Ashwani chandel <ashwanichandel93964@gmail.com>
2025-11-12 21:18:14 +0530

    thirdLargestNumber

--- src/main/java/org/example/Array/ThirdLargestNumber.java
+++ src/main/java/org/example/Array/ThirdLargestNumber.java
@@ -2,8 +2,8 @@ package org.example.Array;
 
 public class ThirdLargestNumber {
     public static void main(String[] args) {
-        int[] arr = {10, 4, 7, 9, 20, 15, 20};
-        thirdLargestNumber(arr);
+        int[] arr = {10, 4, 7, 9, 20, 15, 20,21,22};
+        System.out.println(secondLargest(arr));
     }
 
     static void thirdLargestNumber(int arr[]) {
@@ -27,4 +27,27 @@ public class ThirdLargestNumber {
             System.out.println("Array doesn't have third unique element");
         }
     }
+    static int largestElement(int[] arr){
+        int largest=0;
+        for(int i=0;i<arr.length-1;i++)
+        {
+            if(arr[i]>arr[i+1]){
+                largest=arr[i];
+            }
+        }
+        return largest;
+    }
+    static int secondLargest(int arr[]){
+        Integer first=null,second=null;
+        for(int num:arr){
+            if(first==null||num>first){
+                second=first;
+                first=num;
+            } else if (second==null||num>second&&first!=num) {
+                second=num;
+            }
+        }
+        return second;
+    }
+
 }
