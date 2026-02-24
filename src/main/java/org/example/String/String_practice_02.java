07c6bee90516f75c127ed505fda4902eaabc802b
Ashwani chandel <ashwanichandel93964@gmail.com>
2026-02-24 11:11:14 +0530

    String reverse M2

--- src/main/java/org/example/String/String_practice_02.java
+++ src/main/java/org/example/String/String_practice_02.java
@@ -3,12 +3,19 @@ package org.example.String;
 public class String_practice_02 {
     public static void main(String[] args) {
         String str="Ashwani";
-        reverseString(str);
+        reverseStringM2(str);
     }
     public static void reverseString(String str){
         str=str.toLowerCase();
         for(int i=str.length()-1;i>=0;i--){
             System.out.println(str.charAt(i));
         }
+    }
+    public static void reverseStringM2(String str){
+        str=str.toLowerCase();
+        StringBuilder sb= new StringBuilder();
+        sb.append(str);
+        sb.reverse();
+        System.out.println(sb);
     }
 }
