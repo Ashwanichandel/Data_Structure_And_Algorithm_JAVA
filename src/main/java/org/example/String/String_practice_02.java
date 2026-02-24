1229c75dfcc4f706e99dcdc82d53a7a4281f18a4
Ashwani chandel <ashwanichandel93964@gmail.com>
2026-02-24 16:36:04 +0530

    First non repeating

--- src/main/java/org/example/String/String_practice_02.java
+++ src/main/java/org/example/String/String_practice_02.java
@@ -4,6 +4,7 @@ public class String_practice_02 {
     public static void main(String[] args) {
         String str="Ashwani";
         reverseStringM2(str);
+        System.out.println(findFirstNonRepeatedCharacter("Software Services"));
     }
     public static void reverseString(String str){
         str=str.toLowerCase();
@@ -17,5 +18,24 @@ public class String_practice_02 {
         sb.append(str);
         sb.reverse();
         System.out.println(sb);
+    }
+    public static String findFirstNonRepeatedCharacter(String str){
+        String result="";
+        for(int i=0;i<str.length();i++){
+            int count=0;
+            if(str.charAt(i)!=' '){
+                for(int j=0;j<str.length();j++){
+                    if(str.charAt(i)==str.charAt(j) && i!=j){
+                        count++;
+                        break;
+                    }
+                }
+                if(count==0){
+                    result+=str.charAt(i);
+                    break;
+                }
+            }
+        }
+        return result;
     }
 }
